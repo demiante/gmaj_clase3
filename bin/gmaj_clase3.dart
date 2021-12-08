e58d258fee8d35226b546c1c985ba0c0883a226a
Demiante <demiante@gmail.com>
2021-12-08 12:47:31 -0300

    confirmar en master

---
+++ bin/gmaj_clase3.dart
@@ -0,0 +1,10 @@
+import 'package:gmaj_clase3/gmaj_clase3.dart' as gmaj_clase3;
+
+void main() {
+  int resultado = 50;
+  informarNota(resultado);
+}
+
+void informarNota(int resultado) {
+  print("El resultado es: $resultado");
+}
---
+++ test/gmaj_clase3_test.dart
@@ -0,0 +1,8 @@
+import 'package:gmaj_clase3/gmaj_clase3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
